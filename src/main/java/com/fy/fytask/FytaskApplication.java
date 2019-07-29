edde3c66fd6f18716e5259841be43b82134687ef
xuxl <18366405239@163.com>
2019-07-29 23:09:16 +0800

    Initial commit

---
+++ src/main/java/com/fy/fytask/FytaskApplication.java
@@ -0,0 +1,13 @@
+package com.fy.fytask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FytaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FytaskApplication.class, args);
+    }
+
+}
